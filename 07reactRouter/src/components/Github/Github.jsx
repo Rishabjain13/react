c5aa0cbb01c4809e5b93a7649a61d7a583af3de8
Rishab Jain <rishabjain87723@gmail.com>
2026-02-18 11:37:15 +0530

    redux toolkit

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    const data = useLoaderData()
-    // const [data, setData] = useState([])
-    // useEffect(() => {
-    //  fetch('https://api.github.com/users/Rishabjain13')
-    //  .then(response => response.json())
-    //  .then(data => {
-    //     console.log(data);
-    //     setData(data)
-    //  })
-    // }, [])
-    
+  
+  const data = useLoaderData()
+
+  const githubScore = useMemo(() => {
+    console.log("Calculating GitHub score...");
+    return data.followers * 2 + data.public_repos * 5;
+  }, [data.followers, data.public_repos]);
+
   return (
-    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
-    <img src={data.avatar_url} alt="Git picture" width={300} />
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+      Github Score: {githubScore}
+      <br />
+      Repos: {data.public_repos}
+      <img src={data.avatar_url} alt="Git picture" width={300} />
     </div>
   )
 }
